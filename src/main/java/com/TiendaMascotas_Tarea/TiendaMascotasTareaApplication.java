f4465fe22cdb7d9822e5c2568db50cd15346a6bb
FamMuñozAguilar <FamMuozAguilar@DESKTOP-MBSMDBM>
2024-03-03 21:23:39 -0600

    Proyecto G4 Avance 2

---
+++ src/main/java/com/TiendaMascotas_Tarea/TiendaMascotasTareaApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaMascotas_Tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMascotasTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMascotasTareaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaMascotas_Tarea/TiendaMascotasTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaMascotas_Tarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMascotasTareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
