49ae8ad9344b6157ff98173eedc0384bdc7d95e2
Usuario <Usuario@PC-JP>
2024-04-08 20:24:23 -0600

    Prueba

---
+++ src/test/java/com/ProyectoTiendaVeterinaria/TiendaVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoTiendaVeterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
